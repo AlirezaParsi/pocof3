f8ef35d5ae76e97f8dea761e78c3851a1dd3c09a
Aliza <alirezanajmabadi79@gmail.com>
2025-01-29 15:23:53 +0000

    adding susfs

---
+++ bld.sh
@@ -0,0 +1,8 @@
+export ARCH=arm64
+export SUBARCH=ARM64
+export KBUILD_BUILD_USER="aliza"
+export KBUILD_BUILD_HOST="sad"
+rm -rf out
+export LD_LIBRARY_PATH="~/clang/lib64:$LD_LIBRARY_PATH}"
+make O=out vendor/alioth_defconfig PATH="/workspaces/clang/bin:$PATH"
+make O=out CC=clang -j$(nproc --all) PATH="/workspaces/clang/bin:$PATH" CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1
