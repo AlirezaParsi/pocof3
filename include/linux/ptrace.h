273439d79b507ca2ae219d651e1eec362f5cce8e
Aliza <alirezanajmabadi79@gmail.com>
2025-12-31 12:17:30 +0000

    f

--- include/linux/ptrace.h
+++ include/linux/ptrace.h
@@ -65,9 +65,6 @@ extern void exit_ptrace(struct task_struct *tracer, struct list_head *dead);
 #define PTRACE_MODE_FSCREDS	0x08
 #define PTRACE_MODE_REALCREDS	0x10
 
-#define PTRACE_EVENTMSG_SYSCALL_ENTRY	1
-#define PTRACE_EVENTMSG_SYSCALL_EXIT	2
-
 /* shorthands for READ/ATTACH and FSCREDS/REALCREDS combinations */
 #define PTRACE_MODE_READ_FSCREDS (PTRACE_MODE_READ | PTRACE_MODE_FSCREDS)
 #define PTRACE_MODE_READ_REALCREDS (PTRACE_MODE_READ | PTRACE_MODE_REALCREDS)
@@ -151,7 +148,8 @@ static inline bool ptrace_event_enabled(struct task_struct *task, int event)
 static inline void ptrace_event(int event, unsigned long message)
 {
 	if (unlikely(ptrace_event_enabled(current, event))) {
-	    ptrace_notify((event << 8) | SIGTRAP);
+		current->ptrace_message = message;
+		ptrace_notify((event << 8) | SIGTRAP);
 	} else if (event == PTRACE_EVENT_EXEC) {
 		/* legacy EXEC report via SIGTRAP */
 		if ((current->ptrace & (PT_PTRACED|PT_SEIZED)) == PT_PTRACED)
